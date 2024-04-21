d4a79e0a9eaaadb50f4798488b9907a18e5ae85f
Aleksei Okoneshnikov <AOkoneshnikov@gmail.com>
2024-04-22 08:20:03 +0900

    rfecrc

--- tests/LetsWalletV1R0.spec.ts
+++ tests/LetsWalletV1R0.spec.ts
@@ -1,41 +1,91 @@
 import { Blockchain, SandboxContract, TreasuryContract } from '@ton/sandbox';
-import { toNano } from '@ton/core';
-import { LetsWalletV1R0 } from '../wrappers/LetsWalletV1R0';
+import { beginCell, Cell, toNano } from '@ton/core';
+import { LetsWalletV1R0, WalletOperationV1R0 } from '../wrappers/LetsWalletV1R0';
 import '@ton/test-utils';
+import { keyPairFromSeed, keyPairFromSecretKey, sign, signVerify, KeyPair, getSecureRandomBytes, mnemonicNew, mnemonicToWalletKey } from 'ton-crypto';
+import { TonClient } from '@ton/ton';
 
 describe('LetsWalletV1R0', () => {
     let blockchain: Blockchain;
     let deployer: SandboxContract<TreasuryContract>;
-    let letsWalletV1R0: SandboxContract<LetsWalletV1R0>;
+    let User1: SandboxContract<TreasuryContract>;
+    let User2: SandboxContract<TreasuryContract>;
+    let Sponsor: SandboxContract<TreasuryContract>;
+    let wallet_1: SandboxContract<LetsWalletV1R0>;
+    let wallet_2: SandboxContract<LetsWalletV1R0>;
+    const mnemonicWallet1 =  [
+        'release', 'junior',  'cake',
+        'boost',   'stock',   'illness',
+        'parent',  'move',    'gain',
+        'acid',    'merge',   'search',
+        'female',  'federal', 'apology',
+        'art',     'trade',   'seminar',
+        'message', 'tooth',   'scheme',
+        'broom',   'mention', 'security'
+      ];  
+    const mnemonicWallet2 =  [
+        'cover',   'orient', 'put',
+        'large',   'ivory',  'crowd',
+        'glow',    'sort',   'runway',
+        'laundry', 'lawn',   'price',
+        'prize',   'horror', 'turtle',
+        'elbow',   'cheese', 'shoulder',
+        'village', 'shell',  'jealous',
+        'welcome', 'wink',   'mind'
+      ];
 
-    beforeEach(async () => {
+    beforeEach(async () => {         
+        
+        const keypair_User1: KeyPair = await mnemonicToWalletKey(mnemonicWallet1);
+        const keypair_User2: KeyPair = await mnemonicToWalletKey(mnemonicWallet2);
+    
+
         blockchain = await Blockchain.create();
 
-        letsWalletV1R0 = blockchain.openContract(await LetsWalletV1R0.fromInit());
+        console.log('keypair = ', keypair_User1.publicKey.toString('hex'));
+        let wallet_1_publicKey = BigInt('0x' + keypair_User1.publicKey.toString('hex'));
+
+        console.log('key bigint = ', wallet_1_publicKey);
+        
+        
+
+        wallet_1 = blockchain.openContract(await LetsWalletV1R0.fromInit('RUB', wallet_1_publicKey));
 
         deployer = await blockchain.treasury('deployer');
+        User1 = await blockchain.treasury('User1');
+        User2 = await blockchain.treasury('User2');
 
-        const deployResult = await letsWalletV1R0.send(
+        const deployResult = await wallet_1.send(
             deployer.getSender(),
             {
-                value: toNano('0.05'),
+                value: toNano('5'),
             },
-            {
-                $$type: 'Deploy',
-                queryId: 0n,
-            }
+            null,
         );
-
-        expect(deployResult.transactions).toHaveTransaction({
-            from: deployer.address,
-            to: letsWalletV1R0.address,
-            deploy: true,
-            success: true,
-        });
     });
 
-    it('should deploy', async () => {
-        // the check is done inside beforeEach
-        // blockchain and letsWalletV1R0 are ready to use
+    it('Test #1', async () => {
+        const keypair_User1: KeyPair = await mnemonicToWalletKey(mnemonicWallet1);
+        const keypair_User2: KeyPair = await mnemonicToWalletKey(mnemonicWallet2);
+
+        let data = await wallet_1.getData();
+
+        console.log('wallet_1 data = ', data);
+
+        let wallet_1_operation_data = beginCell()
+                                    .storeUint(0,32)
+                                    .storeUint(1,8)                        
+                                    .endCell();
+
+        console.log(wallet_1_operation_data.toString());
+        const wallet_1_data_signature = sign(Buffer.from(wallet_1_operation_data.toString()), keypair_User1.secretKey); 
+
+        console.log('wallet_1_operation_data = ', wallet_1_operation_data);
+
+        //wallet_1.sendExternal()
+
+
+            
     });
+
 });
--- wrappers/LetsWalletV1R0.compile.ts
+++ wrappers/LetsWalletV1R0.compile.ts
@@ -3,4 +3,8 @@ import { CompilerConfig } from '@ton/blueprint';
 export const compile: CompilerConfig = {
     lang: 'tact',
     target: 'contracts/lets_wallet_v1_r0.tact',
+    options: {
+        debug: true,
+        "external": true
+    }
 };
