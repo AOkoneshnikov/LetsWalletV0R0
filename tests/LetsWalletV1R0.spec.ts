3642d28672e5e4cab2fcd7a5dbf5a3335f0d4abc
AOkoneshnikov <AOkoneshnikov@gmail.com>
2024-04-23 15:59:58 +0900

    sdvsvdsv

--- tests/LetsWalletV1R0.spec.ts
+++ tests/LetsWalletV1R0.spec.ts
@@ -1,11 +1,13 @@
 import { Blockchain, SandboxContract, TreasuryContract } from '@ton/sandbox';
 import { beginCell, Cell, toNano } from '@ton/core';
-import { LetsWalletV1R0, WalletOperationV1R0 } from '../wrappers/LetsWalletV1R0';
+import { LetsWalletV1R0, WalletOperationV1R0, DataSponsor } from '../wrappers/LetsWalletV1R0';
 import '@ton/test-utils';
 import { keyPairFromSeed, keyPairFromSecretKey, sign, signVerify, KeyPair, getSecureRandomBytes, mnemonicNew, mnemonicToWalletKey, sha256 } from 'ton-crypto';
 import { TonClient } from '@ton/ton';
 import { toBigIntBE, toBigIntLE } from '@trufflesuite/bigint-buffer';
 import { Slice } from 'ton';
+import 'json-bigint';
+
 
 describe('LetsWalletV1R0', () => {
     let blockchain: Blockchain;
@@ -37,26 +39,11 @@ describe('LetsWalletV1R0', () => {
       ];
 
     beforeEach(async () => {         
-        
         const keypair_User1: KeyPair = await mnemonicToWalletKey(mnemonicWallet1);
         const keypair_User2: KeyPair = await mnemonicToWalletKey(mnemonicWallet2);
-    
 
         blockchain = await Blockchain.create();
-
-        //console.log('keypair = ', keypair_User1.publicKey.toString('hex'));
-        console.log('public keypair = ',keypair_User1.publicKey);
-        let wallet_1_publicKey = BigInt('0x' + keypair_User1.publicKey.toString('hex'));
-
-        
-        console.log('publicKey (bigint BE) = ', toBigIntBE(keypair_User1.publicKey));
-        console.log('publicKey (bigint LE) = ', toBigIntLE(keypair_User1.publicKey));
-        
-        
-
-        //wallet_1 = blockchain.openContract(await LetsWalletV1R0.fromInit('RUB', toBigIntLE(keypair_User1.publicKey)));
         wallet_1 = blockchain.openContract(await LetsWalletV1R0.fromInit('RUB', toBigIntBE(keypair_User1.publicKey)));
-
 
         deployer = await blockchain.treasury('deployer');
         User1 = await blockchain.treasury('User1');
@@ -76,41 +63,31 @@ describe('LetsWalletV1R0', () => {
         const keypair_User2: KeyPair = await mnemonicToWalletKey(mnemonicWallet2);
 
         let data = await wallet_1.getData();
+        let timeout = Math.floor(Date.now() / 1000) + 20;
 
         console.log('wallet_1 data = ', data);
 
-
-        let wallet_1_operation_data = beginCell()
-                                    .storeUint(0,32)
-                                    .storeUint(3,8)                        
-                                    .endCell();
-    
-
-        const wallet_1_data_signature = sign(wallet_1_operation_data.hash(), keypair_User1.secretKey); 
-        console.log('wallet_1_data_signature = ', toBigIntBE(wallet_1_data_signature));
-
-      ///  let hello_sha = await sha256(wallet_1_operation_data.hash());
-        console.log('hello sha256 = ', toBigIntBE(wallet_1_operation_data.hash()));
-
-
-        wallet_1.sendExternal({
-            $$type: 'CreateTrustlineV1R0',
-            signature: wallet_1_data_signature,
-            debitor: User1.address
-        })
-
-
+        let operation_data = beginCell()
+            .storeUint(0,8)
+            .storeUint(timeout, 32)
+            .storeUint(data.seqno,64)
+            .storeUint(1,8)
+            .storeRef(
+                beginCell()
+                .storeAddress(User2.address)
+                .storeUint(toBigIntBE(keypair_User2.publicKey),256)
+                .endCell()
+            )
+            .endCell();
+        
+        
+        let data_signature = sign(operation_data.hash(), keypair_User1.secretKey); 
+        
         wallet_1.sendExternal({
             $$type: 'WalletOperationV1R0',
-            signature: wallet_1_data_signature,
-            operation: wallet_1_operation_data,
+            signature: data_signature,
+            operation: operation_data,
         });
-
-    
-
-
-        
-       
 
 
             
