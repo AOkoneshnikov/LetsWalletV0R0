72c616d8ce0cd1aa953e8e2e8a62a95be32e19fa
AOkoneshnikov <AOkoneshnikov@gmail.com>
2024-05-07 17:55:39 +0900

    впавпвпп

--- scripts/deployLetsHubV0R0.ts
+++ scripts/deployLetsHubV0R0.ts
@@ -3,14 +3,14 @@ import { LetsHubV0R0 } from '../build/LetsWalletV0R0/tact_LetsHubV0R0';
 import { NetworkProvider } from '@ton/blueprint';
 
 export async function run(provider: NetworkProvider) {
-    const letsHubV0R0 = provider.open(await LetsHubV0R0.fromInit('RUB'));
+    const letsHubV0R0 = provider.open(await LetsHubV0R0.fromInit('TEST'));
 
     console.log(`Hub addres ${letsHubV0R0.address}`);
     
     await letsHubV0R0.send(
         provider.sender(),
         {
-            value: toNano('0.5'),
+            value: toNano('0.1'),
         },
         null
     );
--- scripts/deployLetsWalletV0R0.ts
+++ scripts/deployLetsWalletV0R0.ts
@@ -3,16 +3,19 @@ import { LetsWalletV0R0 } from '../wrappers/LetsWalletV0R0';
 import { NetworkProvider } from '@ton/blueprint';
 
 export async function run(provider: NetworkProvider) {
-    const letsWalletV1R0 = provider.open(await LetsWalletV0R0.fromInit('RUB', 4986475416839105905744086359049208668692504628780654146611869429749505348094n));
+    const letsWalletV1R0 = provider.open(await LetsWalletV0R0.fromInit('RUB', 43244234324n));
 
     console.log(`wallet addres ${letsWalletV1R0.address}`);
     
     await letsWalletV1R0.send(
         provider.sender(),
         {
-            value: toNano('0.5'),
+            value: toNano('0.1'),
         },
-        null
+        {
+            $$type: 'Deploy',
+            queryId: 0n,
+        }
     );
 
     await provider.waitForDeploy(letsWalletV1R0.address);
